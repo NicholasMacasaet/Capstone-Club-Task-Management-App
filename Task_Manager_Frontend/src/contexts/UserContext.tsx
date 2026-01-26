7e8256806d5defdaaf511331320f13be856afba5
NicholasMacasaet <113042065+NicholasMacasaet@users.noreply.github.com>
2026-01-26 15:31:10 -0500

    Setup page routing and user context
    - set up ability to route between pages
    - added context so we can have access to needed data throughout app
    - small test to make sure page routing works

---
+++ Task_Manager_Frontend/src/contexts/UserContext.tsx
@@ -0,0 +1,68 @@
+import { createContext,useContext,useState,useEffect, type ReactNode } from "react";
+import axios from "axios";
+
+type userContextProps = {
+    user:string,
+
+    isLoaded:boolean, 
+
+    loginUser: (username:string) => void,
+
+    logoutUser: () => void
+
+}
+
+const UserContext = createContext<userContextProps|undefined>(undefined)
+
+export const userUserContext = ()=>{
+    const context =  useContext(UserContext)
+
+    if (!context) {
+        throw new Error("UserContext incorrect usage, trying to access context before its defined")
+    }
+    return context
+}
+
+
+export const CrafterContextProvider: React.FC<{children: ReactNode}> = ({children}) => {
+
+    const [user,setUser] = useState("")
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    //check for user token
+    useEffect(()=>{
+        const storedUser = localStorage.getItem('user');
+        if (storedUser) {
+            setUser(JSON.parse(storedUser));
+            setIsLoaded(true)
+        }
+        else {
+            setIsLoaded(true);
+        }
+    },[])
+
+    const loginUser = (username: string) => {
+        console.log('logged in', username)
+        setUser(username);
+        setIsLoaded(true);
+    };
+
+    const logoutUser = () => {
+        console.log('logged out')
+        setUser("");
+        setIsLoaded(true);
+    };
+
+    useEffect(() => {
+    if (user) {
+      localStorage.setItem('user', JSON.stringify(user));
+     
+    }
+  }, [user]);
+
+
+ 
+    return(<UserContext.Provider value ={{user,isLoaded,loginUser,logoutUser}}>
+        {children}
+    </UserContext.Provider>)
+}
---
+++ Task_Manager_Frontend/src/test.tsx
@@ -0,0 +1,10 @@
+
+// testing router
+export const Test = ()=>{
+    return(<>
+        <div className="w-full h-full">
+            <h1>Bingus</h1>
+        </div>
+    
+    </>)
+}
