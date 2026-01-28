ef22f91a00da763da63284f1fca7c3c2306de3b5
NicholasMacasaet <113042065+NicholasMacasaet@users.noreply.github.com>
2026-01-28 14:59:42 -0500

    Sanitization checks & formatting
    - checks for registration, valid emails and passwords, chekcing to see if passwords match
    - tweaks with formatting
    - made login page more consistent with registration page

--- Task_Manager_Frontend/src/pages/login&register/login.tsx
+++ Task_Manager_Frontend/src/pages/login&register/login.tsx
@@ -37,11 +37,33 @@ export const Login = () => {
                 Login to Streamline
             </p>
 
+            <div className="login_form_desktop_wrapper rounded-xl h-1/2 w-3/4 flex justify-center hidden sm:flex ">
+                <form className="h-1/3 w-full sm:w-4/5 mt-1 hidden sm:grid grid-cols-2 items-center mt-20" method="post" onSubmit={(e)=>handleLogin(e)}>
+                
+                    <label htmlFor="username" className="text-xl sm:text-2xl self-center ml-7 sm:ml-0 text-end">Username or Email:</label>
 
-            <form className="login_form h-full w-full sm:w-4/5 rounded-xl mt-1 flex flex-col items-center" method="post" onSubmit={(e)=>handleLogin(e)}>
+                    <input type="text" name="username" className="form_input_modified rounded-xl p-1 text-2xl sm:text-3xl w-4/5 sm:w-full self-center" onChange={(e)=>handleUsernameChange(e)}></input>
 
+                
+                    <label htmlFor="username" className="text-2xl sm:text-3xl self-center ml-7 sm:ml-0 text-end mt-2">Password:</label>
+
+                    <input type="password" name="password" className="form_input_modified rounded-xl p-1 text-2xl sm:text-3xl w-4/5 sm:w-full self-center mt-2" onChange={(e)=>handlePasswordChange(e)}></input>
+
+
+                <div className="col-span-2 flex justify-center items-center flex-col mt-2">
+                    <button type="submit" className="login_submit_button">
+                        Login
+                    </button>
+                </div>
+                
+            </form>
+
+            </div>
+            
+
+            <form className="login_form h-full w-full sm:w-4/5 rounded-xl mt-1 flex sm:hidden flex-col items-center" method="post" onSubmit={(e)=>handleLogin(e)}>
                 <div className="form_group w-full sm:w-2/3 mt-20 flex flex-col sm:flex-row ">
-                    <label htmlFor="username" className="text-2xl sm:text-3xl self-start ml-7 sm:ml-0">Username:</label>
+                    <label htmlFor="username" className="text-xl sm:text-2xl self-start ml-7 sm:ml-0">Username or Email:</label>
 
                     <input type="text" name="username" className="form_input rounded-xl p-1 text-2xl sm:text-3xl w-4/5 sm:w-full self-center" onChange={(e)=>handleUsernameChange(e)}></input>
                 </div>
@@ -59,7 +81,6 @@ export const Login = () => {
                         Login
                     </button>
                 </div>
-                
                 
             </form>
             
--- Task_Manager_Frontend/src/pages/login&register/register.tsx
+++ Task_Manager_Frontend/src/pages/login&register/register.tsx
@@ -1,13 +1,36 @@
-import { useState, type ChangeEvent} from "react";
+import { all } from "axios";
+import { useEffect, useState, type ChangeEvent} from "react";
 
 export const Register = () => {
 
 
     const[username, setUsername] = useState("");
+
     const[email, setEmail] = useState("");
+    const[validEmail, setValidEmail] = useState(true)
+
     const[phone_number, setPhoneNumber] = useState("");
+    const[validPhoneNo, setValidPhoneNo] = useState(true)
+
     const[password, setPassword] = useState("");
+    const[validPassword,setValidPassword] = useState(true)
+
     const[confirm_password, setConfirmPassword] = useState("");
+    const[passwordMatches,setPasswordMatches]= useState(true)
+
+    //https://regexr.com/3e48o
+    const email_regex: RegExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+    //https://webrockstar.net/2017/08/regex-north-american-phone-number/
+    const phone_no_regex: RegExp = /[\s\(]*((?!1)\d{1})((?!11)\d{2})[-\s\).,]*((?!1)\d{1})\d{2}[-\s.,]*\d{4}[\s]*/;
+
+    // Source - https://stackoverflow.com/a
+    // Posted by Srinivas, modified by community. See post 'Timeline' for change history
+    // Retrieved 2026-01-28, License - CC BY-SA 4.0
+    // eight chars, one letter, one number, one special char
+    const password_regex: RegExp = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
+
+
 
     const handleUsernameChange = (event:ChangeEvent<HTMLInputElement>) => {
         event.preventDefault();
@@ -28,6 +51,55 @@ export const Register = () => {
         setConfirmPassword(event.target.value);
     }
 
+    //check if this is a valid email when its nonempty using a regex
+    useEffect(() => {   
+        if (email!==""){
+            setValidEmail(email_regex.test(email))
+        }
+    },[email])
+
+
+    useEffect(()=>{
+        if (phone_number !==""){
+            setValidPhoneNo(phone_no_regex.test(phone_number))
+        }
+    },[phone_number])
+
+    useEffect(()=>{
+        if (password !== ""){
+            setValidPassword(password_regex.test(password))
+        }
+    },[password])
+
+    useEffect(()=>{
+        
+        setPasswordMatches(password === confirm_password)
+        
+    },[password,confirm_password])
+
+    const[error, setError] = useState("")
+
+
+    const handleRegistration = async(event:React.FormEvent<HTMLFormElement>) => {
+
+        event.preventDefault()
+
+        try {
+            console.log("hi")
+
+        }catch(error){
+            console.error(error)
+            if (error instanceof Error) {
+                setError(error.message)
+            }
+        }
+
+    }
+
+    let all_filled = email && password && confirm_password
+
+    let checks_cleared = validEmail && validPhoneNo && validPassword && passwordMatches
+
 
     return(<>
         <div className="login_page w-full h-full flex flex-col justify-start items-center">
@@ -39,83 +111,179 @@ export const Register = () => {
                 Register for Streamline
             </p>
 
-            <div className="login_form_desktop_wrapper h-full sm:h-1/2 w-full sm:w-4/5 rounded-xl mt-1 hidden sm:flex flex-col items-center">
+            <div className="login_form_desktop_wrapper h-full sm:h-3/4 w-full sm:w-4/5 rounded-xl mt-1 hidden sm:flex flex-col items-center">
 
-                <form className="w-full h-3/4 rounded-xl mt-1 hidden sm:grid grid-cols-2" method="post">
+                <form className="w-full h-3/4 rounded-xl mt-1 hidden sm:grid grid-cols-2 " method="post" onSubmit={handleRegistration}>
                  
                     <label htmlFor="username" className="text-lg sm:text-xl self-center ml-7 sm:ml-0 justify-end text-right">Username:</label>
 
-                    <input type="text" name="username" className="form_input_modified rounded-xl p-1 text-lg sm:text-xl w-4/5 sm:w-full self-center" onChange={(e)=>handleUsernameChange(e)}></input>
-                
-
+                    <input type="text" name="username" className="form_input_modified rounded-xl p-1 text-lg sm:text-xl w-4/5 sm:w-full self-center" onChange={(e)=>handleUsernameChange(e)} value={username}></input>
 
                 
-                    <label htmlFor="email" className="text-lg sm:text-xl self-center ml-7 sm:ml-0 text-right">Email:</label>
+                    <label htmlFor="email" className="text-lg sm:text-xl self-center ml-7 sm:ml-0 text-right">Email:*</label>
 
-                    <input type="text" name="email" className="form_input_modified rounded-xl p-1 text-lg sm:text-xl w-4/5 sm:w-full self-center" onChange={(e)=>handleEmailChange(e)}></input>
+                    <input type="text" name="email" className="form_input_modified rounded-xl p-1 text-lg sm:text-xl w-4/5 sm:w-full self-center" onChange={(e)=>handleEmailChange(e)} value={email}placeholder="j.smth@yee.com"></input>
                 
 
                 
                     <label htmlFor="phone_number" className="text-1xl sm:text-xl self-center ml-7 sm:ml-0 whitespace-nowrap text-right">Phone Number:</label>
 
-                    <input type="text" name="phone_number" className="form_input_modified rounded-xl p-1 text-lg sm:text-xl w-4/5 sm:w-full self-center" onChange={(e)=>handlePhoneNumberChange(e)}></input>
+                    <input type="text" name="phone_number" className="form_input_modified rounded-xl p-1 text-lg sm:text-xl w-4/5 sm:w-full self-center" onChange={(e)=>handlePhoneNumberChange(e)} value = {phone_number} placeholder="ex:000-000-0000"></input>
 
-                    <label htmlFor="password" className="text-lg sm:text-xl self-center ml-7 sm:ml-0 text-right">Password:</label>
+                    <label htmlFor="password" className="text-lg sm:text-xl self-center ml-7 sm:ml-0 text-right">Password:*</label>
 
-                    <input type="password" name="password" className="form_input_modified rounded-xl p-1 text-lg sm:text-xl w-4/5 sm:w-full self-center" onChange={(e)=>handlePasswordChange(e)}></input>
+                    <input type="password" name="password" className="form_input_modified rounded-xl p-1 text-lg sm:text-xl w-4/5 sm:w-full self-center" onChange={(e)=>handlePasswordChange(e)} value={password}></input>
+
+                    <p className="text-xs col-span-2">(eight chars, one letter, one number, one special char)</p>
                 
                  
-                    <label htmlFor="confirm_password" className="text-lg sm:text-xl wrap self-center ml-7 sm:ml-0 whitespace-nowrap text-right">Confirm Password:</label>
+                    <label htmlFor="confirm_password" className="text-lg sm:text-xl wrap self-center ml-7 sm:ml-0 whitespace-nowrap text-right">Confirm Password:*</label>
 
-                    <input type="password" name="confirm_password" className="form_input_modified rounded-xl p-1 text-lg sm:text-xl w-4/5 sm:w-full self-center" onChange={(e)=>handleConfirmPasswordChange(e)}></input>
+                    <input type="password" name="confirm_password" className="form_input_modified rounded-xl p-1 text-lg sm:text-xl w-4/5 sm:w-full self-center" onChange={(e)=>handleConfirmPasswordChange(e)} value={confirm_password}></input>
+
+
+                    <div className="col-span-2 flex justify-center items-center flex-col">
+
+                        <div className="w-full flex flex-col errors">
+                            * denotes required fields
+                            {!validEmail &&
+                            email!==""&&
+                            <div className="text-rose-400 flex justify-center">
+                                Error: invalid email
+                            </div>
+
+                            }
+                            {!validPhoneNo&&
+                            phone_number!==""&&
+                            <div className="text-rose-400 flex justify-center">
+                                Error: invalid phone number format
+                            </div>
+                            }
+
+                            {!validPassword&&
+                            password!==""&&
+                            <div className="text-rose-400 flex justify-center">
+                                Error: invalid password
+                            </div>
+                            }
+
+                            {!passwordMatches&&
+                            confirm_password!==""&&
+                            <div className="text-rose-400 flex justify-center">
+                                Error: Passwords doesn't match
+                            </div>
+                            }
+                        </div>
+                        {all_filled&&
+                        checks_cleared&&
+                        <button type="submit" className="login_submit_button mb-1">
+                            Register
+                        </button>
+                        }
+                        {(!all_filled || !checks_cleared)&&
+                        <button className="login_submit_disabled mb-1 text-slate-400" disabled>
+                            Register
+                        </button>
+
+                        }
+                    </div>
                 </form>
 
+                {
+                    error && 
+                    <div className="alert alert-danger" role="alert">
+                        {error}
+                    </div>
+                }
 
-                <button type="submit" className="login_submit_button mb-1">
-                    Login
-                </button>
+        
+
             </div>
 
             
-            <form className="login_form h-full w-full sm:w-4/5 rounded-xl mt-1 flex sm:hidden flex-col items-center" method="post">
-                <div className="form_group w-full sm:w-2/3 mt-20 flex flex-col sm:flex-row ">
+            <form className="login_form h-full w-full sm:w-4/5 rounded-xl mt-1 flex justify-start sm:hidden flex-col justify-start" method="post" onSubmit={handleRegistration}>
+                <div className="form_group w-full sm:w-2/3 mt-10 flex flex-col sm:flex-row ">
                     <label htmlFor="username" className="text-lg sm:text-xl self-start ml-7 sm:ml-0">Username:</label>
 
-                    <input type="text" name="username" className="form_input rounded-xl p-1 text-lg sm:text-xl w-4/5 sm:w-full self-center" onChange={(e)=>handleUsernameChange(e)}></input>
+                    <input type="text" name="username" className="form_input rounded-xl p-1 text-lg sm:text-xl w-4/5 sm:w-full self-center" onChange={(e)=>handleUsernameChange(e)} value={username}></input>
                 </div>
 
 
-                <div className="form_group w-full sm:w-2/3 mt-10 flex flex-col sm:flex-row ">
-                    <label htmlFor="email" className="text-lg sm:text-xl self-start ml-7 sm:ml-0">Email:</label>
+                <div className="form_group w-full mt-10sm:w-2/3 mt-2 flex flex-col sm:flex-row ">
+                    <label htmlFor="email" className="text-lg sm:text-xl self-start ml-7 sm:ml-0">Email:*</label>
 
-                    <input type="text" name="email" className="form_input rounded-xl p-1 text-lg sm:text-xl w-4/5 sm:w-full self-center" onChange={(e)=>handleEmailChange(e)}></input>
+                    <input type="text" name="email" className="form_input rounded-xl p-1 text-lg sm:text-xl w-4/5 sm:w-full self-center" onChange={(e)=>handleEmailChange(e)} value={email} placeholder="j.smth@yee.com"></input>
                 </div>
 
-                <div className="form_group w-full sm:w-2/3 mt-10 flex flex-col sm:flex-row ">
-                    <label htmlFor="phone_number" className="text-1xl sm:text-xl self-start ml-7 sm:ml-0 whitespace-nowrap">Phone Number:</label>
+                <div className="form_group w-full sm:w-2/3 flex mt-2 flex-col sm:flex-row ">
+                    <label htmlFor="phone_number" className="text-1xl sm:text-xl self-start ml-7 sm:ml-0 whitespace-nowrap" >Phone Number:</label>
 
-                    <input type="text" name="phone_number" className="form_input rounded-xl p-1 text-lg sm:text-xl w-4/5 sm:w-full self-center" onChange={(e)=>handlePhoneNumberChange(e)}></input>
-                </div>
-
-
-                <div className="form_group w-full sm:w-2/3 mt-10 flex flex-col sm:flex-row">
-                    <label htmlFor="password" className="text-lg sm:text-xl self-start ml-7 sm:ml-0">Password:</label>
-
-                    <input type="password" name="password" className="form_input rounded-xl p-1 text-lg sm:text-xl w-4/5 sm:w-full self-center" onChange={(e)=>handlePasswordChange(e)}></input>
+                    <input type="text" name="phone_number" className="form_input rounded-xl p-1 text-lg sm:text-xl w-4/5 sm:w-full self-center" onChange={(e)=>handlePhoneNumberChange(e)} value={phone_number} placeholder="ex:000-000-0000"></input>
                 </div>
 
 
-                 <div className="form_group w-full sm:w-2/3 mt-10 flex flex-col sm:flex-row">
-                    <label htmlFor="confirm_password" className="text-lg sm:text-xl wrap self-start ml-7 sm:ml-0 whitespace-nowrap">Confirm Password:</label>
+                <div className="form_group w-full sm:w-2/3 mt-2 flex flex-col sm:flex-row">
+                    <label htmlFor="password" className="text-lg sm:text-xl self-start ml-7 sm:ml-0">Password:*</label>
 
-                    <input type="password" name="confirm_password" className="form_input rounded-xl p-1 text-lg sm:text-xl w-4/5 sm:w-full self-center" onChange={(e)=>handleConfirmPasswordChange(e)}></input>
+                    <input type="password" name="password" className="form_input rounded-xl p-1 text-lg sm:text-xl w-4/5 sm:w-full self-center" onChange={(e)=>handlePasswordChange(e)} value={password}></input>
+
+                    <p className="text-xs m">(eight chars, one letter, one number, one special char)</p>
+                </div>
+
+
+                 <div className="form_group w-full mt-2 sm:w-2/3 flex flex-col sm:flex-row">
+                    <label htmlFor="confirm_password" className="text-lg sm:text-xl wrap self-start ml-7 sm:ml-0 whitespace-nowrap" >Confirm Password:*</label>
+
+                    <input type="password" name="confirm_password" className="form_input rounded-xl p-1 text-lg sm:text-xl w-4/5 sm:w-full self-center" onChange={(e)=>handleConfirmPasswordChange(e)} value={confirm_password}></input>
+                </div>
+
+                <div className="w-full flex flex-col errors">
+                    * denotes required fields
+                    {!validEmail &&
+                    email!==""&&
+                    <div className="text-rose-400 flex justify-center">
+                        Error: invalid email
+                    </div>
+
+                    }
+                    {!validPhoneNo&&
+                    phone_number!==""&&
+                    <div className="text-rose-400 flex justify-center">
+                        Error: invalid phone number format
+                    </div>
+                    }
+
+                    {!validPassword&&
+                    password!==""&&
+                    <div className="text-rose-400 flex justify-center">
+                        Error: invalid password
+                    </div>
+                    }
+
+                    {!passwordMatches&&
+                    confirm_password!==""&&
+                    <div className="text-rose-400 flex justify-center">
+                        Error: Passwords doesn't match
+                    </div>
+                    }
                 </div>
 
 
                 <div className="form_group w-full sm:w-2/3 mt-10 flex justify-center">
-                    <button type="submit" className="login_submit_button">
-                        Login
-                    </button>
+                    {
+                        all_filled&&
+                        checks_cleared&&
+                        <button type="submit" className="login_submit_button mb-1">
+                            Register
+                        </button>
+                        }
+
+                        {(!all_filled || !checks_cleared)&&
+                        <button className="login_submit_disabled mb-1 text-slate-400" disabled>
+                            Register
+                        </button>
+
+                        }
                 </div>
             </form>
             
